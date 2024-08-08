06f832c06d0d1cef2fabeb299ee4156e84841f51
Avrians <avriansyahb10@gmail.com>
2024-08-08 08:50:37 +0700

    [av][add di]

--- app/src/main/java/com/avrians/tilikbrebes/di/injection.kt
+++ app/src/main/java/com/avrians/tilikbrebes/di/injection.kt
@@ -1,4 +1,9 @@
 package com.avrians.tilikbrebes.di
 
+import com.avrians.tilikbrebes.data.WisataRepository
+
 object injection {
+    fun provideRepository(): WisataRepository {
+        return WisataRepository.getInstance()
+    }
 }
