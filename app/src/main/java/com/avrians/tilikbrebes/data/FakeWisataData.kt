d8eeddcbf35596c08a56e7621e60d0c92ca0fab8
Avrians <avriansyahb10@gmail.com>
2024-08-08 08:43:19 +0700

    [av][add model and fake data]

--- app/src/main/java/com/avrians/tilikbrebes/data/FakeWisataData.kt
+++ app/src/main/java/com/avrians/tilikbrebes/data/FakeWisataData.kt
@@ -1,4 +1,128 @@
 package com.avrians.tilikbrebes.data
 
+import com.avrians.tilikbrebes.R
+import com.avrians.tilikbrebes.model.Wisata
+
 object FakeWisataData {
+    val dummyWisata = listOf(
+        Wisata(
+            id = 1,
+            name = "Pantai Tanjung Kait",
+            location = "Tanjung Kait, Brebes",
+            description = "Pantai Tanjung Kait adalah salah satu pantai yang berada di Kabupaten Brebes, Jawa Tengah. Pantai ini memiliki pasir putih yang lembut dan air laut yang jernih. Pantai Tanjung Kait juga memiliki bebatuan besar yang menjorok ke laut. Bebatuan ini sering dijadikan tempat duduk oleh pengunjung yang ingin menikmati pemandangan laut.",
+            image = R.drawable.tanjungkait,
+            rating = 4.5,
+            price = "Rp 10.000",
+            openTime = "08.00",
+            closeTime = "17.00"
+        ),
+
+        Wisata(
+            id = 2,
+            name = "Waduk Malahayu",
+            location = "Malahayu, Banjarharjo",
+            description = "Waduk Malahayu adalah waduk yang terletak di Kecamatan Banjarharjo, Kabupaten Brebes. Waduk ini dikelilingi oleh pegunungan dan hutan yang hijau, membuatnya menjadi tempat yang ideal untuk rekreasi keluarga. Pengunjung dapat menikmati pemandangan indah, memancing, atau naik perahu di waduk ini.",
+            image = R.drawable.malahayu,
+            rating = 4.3,
+            price = "Rp 5.000",
+            openTime = "06.00",
+            closeTime = "18.00"
+        ),
+
+        Wisata(
+            id = 3,
+            name = "Curug Putri",
+            location = "Bumiayu, Brebes",
+            description = "Curug Putri adalah air terjun yang terletak di daerah Bumiayu, Kabupaten Brebes. Air terjun ini memiliki ketinggian sekitar 25 meter dengan air yang jernih dan segar. Di sekitar air terjun, terdapat pepohonan hijau yang rindang, membuat suasana menjadi sejuk dan asri.",
+            image = R.drawable.curugputri,
+            rating = 4.7,
+            price = "Rp 7.000",
+            openTime = "07.00",
+            closeTime = "16.00"
+        ),
+
+        Wisata(
+            id = 4,
+            name = "Pantai Randusanga Indah",
+            location = "Randusanga, Brebes",
+            description = "Pantai Randusanga Indah adalah destinasi wisata pantai yang populer di Brebes. Pantai ini menawarkan pemandangan laut yang luas dengan pasir hitam yang halus. Fasilitas di pantai ini cukup lengkap, mulai dari area bermain anak, warung makan, hingga tempat berkemah.",
+            image = R.drawable.randusanga,
+            rating = 4.2,
+            price = "Rp 15.000",
+            openTime = "08.00",
+            closeTime = "18.00"
+        ),
+
+        Wisata(
+            id = 5,
+            name = "Bukit Panenjoan",
+            location = "Salem, Brebes",
+            description = "Bukit Panenjoan adalah destinasi wisata alam yang terletak di Kecamatan Salem, Brebes. Bukit ini menawarkan pemandangan alam yang indah dengan hamparan pepohonan dan sawah yang hijau. Dari puncak bukit, pengunjung dapat melihat pemandangan matahari terbit dan terbenam yang memukau.",
+            image = R.drawable.bukitPanenjoan,
+            rating = 4.8,
+            price = "Rp 3.000",
+            openTime = "05.00",
+            closeTime = "19.00"
+        ),
+
+        Wisata(
+            id = 6,
+            name = "Hutan Mangrove Pandansari",
+            location = "Pandansari, Brebes",
+            description = "Hutan Mangrove Pandansari adalah kawasan konservasi mangrove yang terletak di Pandansari, Brebes. Tempat ini menawarkan pemandangan hutan mangrove yang hijau dan berbagai jenis satwa. Pengunjung dapat berjalan-jalan di jembatan kayu yang dibangun di atas hutan mangrove sambil menikmati keindahan alam.",
+            image = R.drawable.mangrovepandansari,
+            rating = 4.4,
+            price = "Rp 10.000",
+            openTime = "08.00",
+            closeTime = "17.00"
+        ),
+
+        Wisata(
+            id = 7,
+            name = "Agrowisata Kebun Teh Kaligua",
+            location = "Paguyangan, Brebes",
+            description = "Agrowisata Kebun Teh Kaligua adalah kebun teh yang terletak di Paguyangan, Brebes. Pengunjung dapat melihat proses pengolahan teh, berjalan-jalan di kebun teh yang luas, dan menikmati pemandangan perbukitan yang indah. Tempat ini juga menyediakan berbagai fasilitas rekreasi dan edukasi.",
+            image = R.drawable.kaligua,
+            rating = 4.6,
+            price = "Rp 20.000",
+            openTime = "07.00",
+            closeTime = "17.00"
+        ),
+
+        Wisata(
+            id = 8,
+            name = "Museum RA Kartini",
+            location = "Bumiayu, Brebes",
+            description = "Museum RA Kartini adalah museum yang didedikasikan untuk mengenang perjuangan RA Kartini dalam memperjuangkan emansipasi wanita di Indonesia. Museum ini terletak di Bumiayu, Brebes, dan menyimpan berbagai koleksi yang berkaitan dengan kehidupan dan perjuangan RA Kartini.",
+            image = R.drawable.museum,
+            rating = 4.1,
+            price = "Rp 5.000",
+            openTime = "08.00",
+            closeTime = "16.00"
+        ),
+
+        Wisata(
+            id = 9,
+            name = "Pemandian Air Panas Cipanas Buaran",
+            location = "Buaran, Brebes",
+            description = "Pemandian Air Panas Cipanas Buaran adalah pemandian air panas alami yang terletak di Buaran, Brebes. Tempat ini menawarkan kolam pemandian dengan air panas yang berasal dari sumber alami, yang dipercaya memiliki berbagai manfaat kesehatan. Pengunjung dapat bersantai dan menikmati kehangatan air panas.",
+            image = R.drawable.cipanasbuaran,
+            rating = 4.5,
+            price = "Rp 10.000",
+            openTime = "06.00",
+            closeTime = "20.00"
+        ),
+
+        Wisata(
+            id = 10,
+            name = "Bukit Baper",
+            location = "Kersana, Brebes",
+            description = "Bukit Baper adalah bukit yang terletak di Kecamatan Kersana, Brebes. Bukit ini terkenal dengan pemandangan alamnya yang indah dan udara yang segar. Dari puncak Bukit Baper, pengunjung dapat melihat hamparan sawah, pegunungan, dan pemandangan kota Brebes yang menakjubkan.",
+            image = R.drawable.bukitbaper,
+            rating = 4.7,
+            price = "Rp 5.000",
+            openTime = "05.00",
+            closeTime = "19.00"
+        )
+    )
 }
--- app/src/main/java/com/avrians/tilikbrebes/model/Wisata.kt
+++ app/src/main/java/com/avrians/tilikbrebes/model/Wisata.kt
@@ -1,5 +1,14 @@
 package com.avrians.tilikbrebes.model
 
 data class Wisata(
-    val id: String,
+    val id: Int,
+    val name: String,
+    val location: String,
+    val description: String,
+    val image: Int,
+    val rating: Double,
+    val price: String,
+    val openTime: String,
+    val closeTime: String,
+    val isFavorite: Boolean = false
 )
